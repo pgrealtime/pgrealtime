dc810a8e7580871cc324a4dd92e526a396157f95
daveycodez <daveycodez@gmail.com>
2025-11-20 16:28:13 -0800

    Add spacing for readability in db.ts
    Inserted a blank line for improved code readability and consistency in the database initialization file.

--- apps/web/src/database/db.ts
+++ apps/web/src/database/db.ts
@@ -6,5 +6,6 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set in the .env file")
 }
 
+
 const sql = neon(process.env.DATABASE_URL)
 export const db = drizzle(sql, { schema })
