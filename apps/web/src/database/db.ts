7710d74bba180d385c7b54f03b37d1ec01699ca0
daveycodez <daveycodez@gmail.com>
2025-11-20 23:50:39 -0800

    Add debug log for FOO environment variable
    Added a console.log statement to output the value of process.env.FOO for debugging purposes.

--- apps/web/src/database/db.ts
+++ apps/web/src/database/db.ts
@@ -3,5 +3,6 @@ import { neon } from "@neondatabase/serverless"
 import { drizzle } from "drizzle-orm/neon-http"
 import * as schema from "./schema"
 
+console.log(process.env.FOO)
 const sql = neon(env.DATABASE_URL || "postgres://foo:bar@localhost")
 export const db = drizzle(sql, { schema })
